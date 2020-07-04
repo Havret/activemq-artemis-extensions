487733912a1852ba6e12039cfd12e8c0bc74c138
Havret <h4vret@gmail.com>
2020-07-04 19:09:01 +0200

    Add proper support for multiple producers

--- src/ActiveMQ.Artemis.Client.Extensions.AspNetCore/ActiveMqHostedService.cs
+++ src/ActiveMQ.Artemis.Client.Extensions.AspNetCore/ActiveMqHostedService.cs
@@ -9,23 +9,30 @@ namespace ActiveMQ.Artemis.Client.Extensions.AspNetCore
     {
         private readonly IEnumerable<ActiveMqTopologyManager> _topologyManagers;
         private readonly IEnumerable<ActiveMqConsumer> _consumers;
+        private readonly IEnumerable<IProducerInitializer> _producerInitializers;
 
-        public ActiveMqHostedService(IEnumerable<ActiveMqTopologyManager> topologyManagers, IEnumerable<ActiveMqConsumer> consumers)
+        public ActiveMqHostedService(IEnumerable<ActiveMqTopologyManager> topologyManagers, IEnumerable<ActiveMqConsumer> consumers, IEnumerable<IProducerInitializer> producerInitializers)
         {
             _topologyManagers = topologyManagers;
             _consumers = consumers;
+            _producerInitializers = producerInitializers;
         }
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
+            foreach (var producer in _producerInitializers)
+            {
+                await producer.Initialize(cancellationToken).ConfigureAwait(false);
+            }
+            
             foreach (var activeMqTopologyManager in _topologyManagers)
             {
-                await activeMqTopologyManager.CreateTopologyAsync(cancellationToken);
+                await activeMqTopologyManager.CreateTopologyAsync(cancellationToken).ConfigureAwait(false);
             }
 
             foreach (var activeMqConsumer in _consumers)
             {
-                await activeMqConsumer.StartAsync(cancellationToken);
+                await activeMqConsumer.StartAsync(cancellationToken).ConfigureAwait(false);
             }
         }
 
---
+++ src/ActiveMQ.Artemis.Client.Extensions.AspNetCore/TypedActiveMqProducer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ActiveMQ.Artemis.Client.Transactions;
+
+namespace ActiveMQ.Artemis.Client.Extensions.AspNetCore
+{
+    internal class TypedActiveMqProducer<T> : IProducer, IProducerInitializer
+    {
+        private IProducer _producer;
+        private readonly Func<CancellationToken, Task<IProducer>> _producerFactory;
+
+        public TypedActiveMqProducer(Func<CancellationToken, Task<IProducer>> producerFactory)
+        {
+            _producerFactory = producerFactory;
+        }
+
+        async ValueTask IProducerInitializer.Initialize(CancellationToken cancellationToken)
+        {
+            if (_producer != null)
+            {
+                throw new InvalidOperationException($"Producer with type {typeof(T).FullName} has already been initialized.");
+            }
+            
+            _producer = await _producerFactory(cancellationToken);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_producer != null)
+            {
+                await _producer.DisposeAsync();                
+            }
+        }
+
+        public Task SendAsync(Message message, Transaction transaction, CancellationToken cancellationToken)
+        {
+            CheckState();
+            return _producer.SendAsync(message, transaction, cancellationToken);
+        }
+
+        public void Send(Message message, CancellationToken cancellationToken)
+        {
+            CheckState();
+            _producer.Send(message, cancellationToken);
+        }
+
+        private void CheckState()
+        {
+            if (_producer == null)
+            {
+                throw new InvalidOperationException("Producer was not initialized.");
+            }
+        }
+    }
+}
