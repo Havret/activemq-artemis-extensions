e9619924399e587c635e00a25e233769f9aeedcf
Havret <h4vret@gmail.com>
2020-07-04 19:07:06 +0200

    Add TestFixture to streamline adding new tests

---
+++ test/ActiveMQ.Artemis.Client.Extensions.AspNetCore.Tests/TestFixture.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ActiveMQ.Artemis.Client.Extensions.AspNetCore.Tests
+{
+    public class TestFixture : IAsyncDisposable
+    {
+        private readonly IHost _host;
+
+        private TestFixture(IHost host, IConnection connection)
+        {
+            _host = host;
+            Connection = connection;
+        }
+
+        public static async Task<TestFixture> CreateAsync(Action<IActiveMqBuilder> configureActiveMq = null, Action<IServiceCollection> configureServices = null)
+        {
+            var host = new HostBuilder()
+                       .ConfigureWebHost(webBuilder =>
+                       {
+                           webBuilder
+                               .ConfigureServices(services =>
+                               {
+                                   services.AddSingleton<IServer>(serviceProvider => new TestServer(serviceProvider));
+                                   configureServices?.Invoke(services);
+                                   configureActiveMq?.Invoke(services.AddActiveMq());
+                               })
+                               .Configure(app => { });
+                       })
+                       .Build();
+            await host.StartAsync();
+
+            var connectionFactory = new ConnectionFactory();
+            var endpoint = Endpoint.Create(host: "localhost", port: 5672, "guest", "guest");
+            var connection = await connectionFactory.CreateAsync(endpoint);
+
+            return new TestFixture(host, connection);
+        }
+
+        public IServiceProvider Services => _host.Services;
+        public IConnection Connection { get; }
+
+        public async ValueTask DisposeAsync()
+        {
+            await Connection.DisposeAsync();
+            await _host.StopAsync();
+            _host.Dispose();
+        }
+    }
+}
