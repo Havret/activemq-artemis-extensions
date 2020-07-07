eb2b4c181ab89d4f851564446c2780bff886b634
Havret <h4vret@gmail.com>
2020-07-07 23:53:55 +0200

    Support consumer and queue configuration

--- src/ActiveMQ.Artemis.Client.Extensions.AspNetCore/ActiveMqTopologyManager.cs
+++ src/ActiveMQ.Artemis.Client.Extensions.AspNetCore/ActiveMqTopologyManager.cs
@@ -10,22 +10,30 @@ namespace ActiveMQ.Artemis.Client.Extensions.AspNetCore
     {
         private readonly AsyncValueLazy<IConnection> _lazyConnection;
         private readonly List<QueueConfiguration> _queueConfigurations;
+        private readonly Dictionary<string, HashSet<RoutingType>> _addressConfigurations;
 
-        public ActiveMqTopologyManager(AsyncValueLazy<IConnection> lazyConnection, List<QueueConfiguration> queueConfigurations)
+        public ActiveMqTopologyManager(AsyncValueLazy<IConnection> lazyConnection, List<QueueConfiguration> queueConfigurations, Dictionary<string, HashSet<RoutingType>> addressConfigurations)
         {
             _lazyConnection = lazyConnection;
             _queueConfigurations = queueConfigurations;
+            _addressConfigurations = addressConfigurations;
         }
 
         public async Task CreateTopologyAsync(CancellationToken cancellationToken)
         {
-            if (_queueConfigurations.Count == 0)
+            if (_queueConfigurations.Count == 0 && _addressConfigurations.Count == 0)
             {
                 return;
             }
 
             var connection = await _lazyConnection.GetValueAsync(cancellationToken).ConfigureAwait(false);
             await using var topologyManager = await connection.CreateTopologyManagerAsync(cancellationToken).ConfigureAwait(false);
+
+            foreach (var addressConfiguration in _addressConfigurations)
+            {
+                await topologyManager.DeclareAddressAsync(addressConfiguration.Key, addressConfiguration.Value, cancellationToken);
+            }
+            
             var queues = await topologyManager.GetQueueNamesAsync(cancellationToken).ConfigureAwait(false);
             foreach (var queueConfiguration in _queueConfigurations.Where(x => !queues.Contains(x.Name)))
             {
