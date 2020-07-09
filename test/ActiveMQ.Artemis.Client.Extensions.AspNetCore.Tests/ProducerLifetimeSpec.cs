8d1beb5d9ea94188553a2d91bc11158467b54e1e
Havret <h4vret@gmail.com>
2020-07-09 22:46:54 +0200

    Support service lifetime configuration for message producers

---
+++ test/ActiveMQ.Artemis.Client.Extensions.AspNetCore.Tests/ProducerLifetimeSpec.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace ActiveMQ.Artemis.Client.Extensions.AspNetCore.Tests
+{
+    public class ProducerLifetimeSpec
+    {
+        [Fact]
+        public async Task Should_register_producer_with_transient_service_lifetime_by_default_1()
+        {
+            await ShouldRegisterProducerWithTransientServiceLifetimeByDefault(builder => builder.AddProducer<TestProducer>(Guid.NewGuid().ToString()));
+        }
+
+        [Fact]
+        public async Task Should_register_producer_with_transient_service_lifetime_by_default_2()
+        {
+            await ShouldRegisterProducerWithTransientServiceLifetimeByDefault(builder => builder.AddProducer<TestProducer>(Guid.NewGuid().ToString(), RoutingType.Multicast));
+        }
+
+        [Fact]
+        public async Task Should_register_producer_with_transient_service_lifetime_by_default_3()
+        {
+            await ShouldRegisterProducerWithTransientServiceLifetimeByDefault(builder => builder.AddProducer<TestProducer>(Guid.NewGuid().ToString(), RoutingType.Multicast, new ProducerOptions()
+            {
+                MessagePriority = 9
+            }));
+        }
+
+        private static async Task ShouldRegisterProducerWithTransientServiceLifetimeByDefault(Action<IActiveMqBuilder> registerProducerAction)
+        {
+            await using var testFixture = await TestFixture.CreateAsync(registerProducerAction);
+
+            var typedProducer1 = testFixture.Services.GetService<TestProducer>();
+            var typedProducer2 = testFixture.Services.GetService<TestProducer>();
+
+            Assert.NotEqual(typedProducer1, typedProducer2);
+            Assert.Equal(typedProducer1.Producer, typedProducer2.Producer);
+        }
+
+        [Fact]
+        public async Task Should_register_producer_with_singleton_service_lifetime_1()
+        {
+            await ShouldRegisterProducerWithSingletonServiceLifetime(builder => builder.AddProducer<TestProducer>(Guid.NewGuid().ToString(), ServiceLifetime.Singleton));
+        }
+
+        [Fact]
+        public async Task Should_register_producer_with_singleton_service_lifetime_2()
+        {
+            await ShouldRegisterProducerWithSingletonServiceLifetime(builder => builder.AddProducer<TestProducer>(Guid.NewGuid().ToString(), RoutingType.Multicast, ServiceLifetime.Singleton));
+        }
+
+        [Fact]
+        public async Task Should_register_producer_with_singleton_service_lifetime_3()
+        {
+            await ShouldRegisterProducerWithSingletonServiceLifetime(builder => builder.AddProducer<TestProducer>(Guid.NewGuid().ToString(), RoutingType.Multicast, new ProducerOptions
+            {
+                MessagePriority = 9
+            }, ServiceLifetime.Singleton));
+        }
+
+        private static async Task ShouldRegisterProducerWithSingletonServiceLifetime(Action<IActiveMqBuilder> registerProducerAction)
+        {
+            await using var testFixture = await TestFixture.CreateAsync(registerProducerAction);
+
+            var typedProducer1 = testFixture.Services.GetService<TestProducer>();
+            var typedProducer2 = testFixture.Services.GetService<TestProducer>();
+
+            Assert.Equal(typedProducer1, typedProducer2);
+            Assert.Equal(typedProducer1.Producer, typedProducer2.Producer);
+        }
+
+        [Fact]
+        public async Task Should_register_producer_with_scoped_service_lifetime_1()
+        {
+            await ShouldRegisterProducerWithScopedServiceLifetime(builder => builder.AddProducer<TestProducer>(Guid.NewGuid().ToString(), ServiceLifetime.Scoped));
+        }
+
+        [Fact]
+        public async Task Should_register_producer_with_scoped_service_lifetime_2()
+        {
+            await ShouldRegisterProducerWithScopedServiceLifetime(builder => builder.AddProducer<TestProducer>(Guid.NewGuid().ToString(), RoutingType.Multicast, ServiceLifetime.Scoped));
+        }
+
+        [Fact]
+        public async Task Should_register_producer_with_scoped_service_lifetime_3()
+        {
+            await ShouldRegisterProducerWithScopedServiceLifetime(builder => builder.AddProducer<TestProducer>(Guid.NewGuid().ToString(), RoutingType.Multicast, new ProducerOptions
+            {
+                MessagePriority = 9
+            }, ServiceLifetime.Scoped));
+        }
+
+        private static async Task ShouldRegisterProducerWithScopedServiceLifetime(Action<IActiveMqBuilder> registerProducerAction)
+        {
+            await using var testFixture = await TestFixture.CreateAsync(registerProducerAction);
+            
+            using var scope = testFixture.Services.CreateScope();
+            var typedProducer1Scope1 = scope.ServiceProvider.GetService<TestProducer>();
+            var typedProducer2Scope1 = scope.ServiceProvider.GetService<TestProducer>();
+            
+            using var scope2 = testFixture.Services.CreateScope();
+            var typedProducerScope2 = scope2.ServiceProvider.GetService<TestProducer>();
+            
+            Assert.Equal(typedProducer1Scope1, typedProducer2Scope1);
+            Assert.Equal(typedProducer1Scope1.Producer, typedProducer2Scope1.Producer);
+            Assert.NotEqual(typedProducerScope2, typedProducer1Scope1);
+            Assert.Equal(typedProducerScope2.Producer, typedProducer2Scope1.Producer);
+        }
+
+        private class TestProducer
+        {
+            public IProducer Producer { get; }
+            public TestProducer(IProducer producer) => Producer = producer;
+        }
+    }
+}
